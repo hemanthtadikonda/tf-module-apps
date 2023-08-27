8d94c8904473af5247da148b4e3318dd26d00f66
hemanth <hemanthtadikonda29@gmail.com>
2023-08-27 17:04:16 +0530

    tf -module -apps

--- userdata.sh
+++ userdata.sh
@@ -2,4 +2,4 @@
 
 yum install ansible -y &>>/opt/userdata.log
 
-ansible-pull -i localhost, -U https://github.com/hemanthtadikonda/Ansible-roboshop.git main.yml -e component = ${component} &>>/opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/hemanthtadikonda/Ansible-roboshop.git main.yml -e component=${component} &>>/opt/userdata.log
