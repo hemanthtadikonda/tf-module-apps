e86e82cfc191c9a80364b172960a85de6b92c417
hemanth <hemanthtadikonda29@gmail.com>
2023-08-27 16:53:39 +0530

    tf -module -apps

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_launch_template" "main" {
   name                   = "${local.name_prefix}-template"
   image_id               = data.aws_ami.ami.id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.main]
+  vpc_security_group_ids = [aws_security_group.main.id]
   user_data              = base64encode(templatefile("${path.module}/userdata.sh", { component = var.component } ))
   tag_specifications {
     resource_type        = "instance"
