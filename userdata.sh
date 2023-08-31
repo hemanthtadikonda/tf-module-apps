3a52b0a81ea57570b8434846d8cc8f96b3dfa1b8
hemanth <hemanthtadikonda29@gmail.com>
2023-08-31 10:31:59 +0530

    ansible - roboshop -path change

--- userdata.sh
+++ userdata.sh
@@ -6,4 +6,4 @@ yum install ansible -y &>>/opt/userdata.log
 
 pip3.11 install botocore boto3   &>>/opt/userdata.log
 
-ansible-pull -i localhost, -U https://github.com/hemanthtadikonda/roboshop-ansible.git main.yml -e component=${component}  &>>/opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/hemanthtadikonda/roboshop-ansible.git main.yml -e component=${component} -e env=${env} &>>/opt/userdata.log
